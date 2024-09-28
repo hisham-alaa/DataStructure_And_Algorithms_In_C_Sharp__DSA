6acfdc5371f45a93767e7a76eaa8eca0ce546bec
HishamAlaa <hishamalaa923@gmail.com>
2024-09-28 20:39:22 +0300

    some other problems solved

--- ProblemSolving_LeetCode/ArrayStringProblems.cs
+++ ProblemSolving_LeetCode/ArrayStringProblems.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,7 +46,98 @@ namespace ProblemSolving_LeetCode
 
         }
 
+        #region Remove Duplicates from Sorted Array
+        //Link To the Problem : https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+        /*
+         * a loop on this array and check only different consecutive values can solve this problem thats it
+         */
+        #endregion
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int counter = 1;
 
+            for (int i = 1; i < nums.Count(); i++)
+                if (nums[i] != nums[i - 1])
+                    nums[counter++] = nums[i];
+
+            return counter;
+        }
+
+
+        public static int RemoveDuplicatesSecondVersion(int[] nums)
+        {
+            int counter = 0, occurences = 0;
+
+            for (int i = 0; i < nums.Count() - 1; i++)
+                if (nums[i] != nums[i + 1])
+                {
+                    nums[counter++] = nums[i];
+                    if (occurences > 0)
+                    {
+                        nums[counter++] = nums[i];
+                        occurences = 0;
+                    }
+                    if (i == nums.Length - 2)
+                    {
+                        nums[counter++] = nums[i + 1];
+                    }
+                }
+                else
+                    occurences++;
+
+            return counter;
+        }
+
+        public static int Candy(int[] ratings)
+        {
+            int TotalCandies = ratings.Length;
+
+            if (ratings[ratings.Length - 1] > ratings[ratings.Length - 2])
+                TotalCandies++;
+
+            if (ratings[0] > ratings[1])
+                TotalCandies++;
+
+
+            for (int i = 1; i < ratings.Length - 1; i++)
+                if (ratings[i] > ratings[i + 1] || ratings[i] > ratings[i - 1])
+                    TotalCandies++;
+
+            return TotalCandies;
+        }
+
+        public static IList<string> FizzBuzz(int n)
+        {
+            List<string> res = new List<string>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                switch (i % 15)// switch goes directly to the res so it must be faster than using if statement
+                {
+                    case 0:
+                        res.Add("FizzBuzz");
+                        break;
+
+                    case 3:
+                    case 6:
+                    case 9:
+                        res.Add("Fizz");
+                        break;
+
+                    case 5:
+                    case 10:
+                        res.Add("Buzz");
+                        break;
+
+                    default:
+                        res.Add($"{i}");
+                        break;
+                }
+
+            }
+
+            return res;
+        }
 
     }
 }
--- ProblemSolving_LeetCode/Program.cs
+++ ProblemSolving_LeetCode/Program.cs
@@ -8,7 +8,9 @@ namespace ProblemSolving_LeetCode
         {
 
             //ArrayStringProblems.Merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
-
+            //ArrayStringProblems.RemoveDuplicatesSecondVersion([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]);
+            //ArrayStringProblems.Candy([1, 0, 2]);
+            ArrayStringProblems.FizzBuzz(15);
         }
     }
 }
